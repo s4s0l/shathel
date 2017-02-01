380ca3785e41524aabda3e8faa1d71dd8cb816c5
SaSol <mwielgus@outlook.com>
2017-02-01 13:36:28 +0100

    playing with shipable

--- dockerBuild.sh
+++ dockerBuild.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
 set -e
 docker build -t dind4java:1.13.0 sandbox/did4java
-docker run --privileged --name some-docker -w /build -v $(pwd):/build --rm -ti dind4java:1.13.0 ./gradlew test
-docker run --privileged --name some-docker -w /build -v $(pwd):/build --rm -ti dind4java:1.13.0 ./gradlew clean
+docker run --privileged -w /build -v $(pwd):/build --rm dind4java:1.13.0 ./gradlew --gradle-user-home=.cache --project-cache-dir=.cache clean test clean
